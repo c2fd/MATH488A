b66df27080c229456226f51b2e53ab653eb2a4c0
JiaZhaoMath <89438129+JiaZhaoMath@users.noreply.github.com>
2024-03-27 18:42:59 -0400

    Update Algorithm-11-3.py

--- Algorithm-11-3.py
+++ Algorithm-11-3.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def least_squares_SVD(A, b):
     U, S, Vt = np.linalg.svd(A, full_matrices=False)
     Sigma = np.diag(S)
@@ -6,7 +7,6 @@ def least_squares_SVD(A, b):
     w = np.linalg.solve(Sigma, Utb)
     x = Vt.T@w
     return x
-
 
 A = np.array([[1, -6], [1, -2], [1, 1], [1, 7]])
 b = np.array([[-1],[2],[1],[6]])
