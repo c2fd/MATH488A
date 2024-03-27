0a5b8c51e8780fa29c246eeb1471c39011e1adbd
JiaZhaoMath <89438129+JiaZhaoMath@users.noreply.github.com>
2024-03-27 18:40:37 -0400

    Update Algorithm-10-1.py

--- Algorithm-10-1.py
+++ Algorithm-10-1.py
@@ -4,7 +4,7 @@ import math
 
 sign = lambda x: math.copysign(1, x)
 
-def HouseHolder_QR(A_ori):
+def HouseHolder_QR_withoutQ(A_ori):
     m, n = A_ori.shape
     A = np.array(A_ori)
 
@@ -37,27 +37,12 @@ def HouseHolder_QR_withQ(A_ori):
     R = A
     return Q, R
 
-# def HouseHolder_QR(A, b):
-#     m, n = A.shape
-#     for k in range(n):
-#         x = A[k:m,k]
-#         #x = x.reshape(-1,1)
-#         print('x',x)
-#         e1 = 0*x
-#         e1[0] = 1
-#         vk = sign(x[0]) * norm(x,2) * e1 + x
-#         print('norm 2', norm(x,2))
-#         vk = vk / norm(vk, 2)
-#         A[k:m,k:n] = A[k:m,k:n] - 2 * np.outer(vk,vk) @ A [k:m,k:n]
-#         b[k:m] = b[k:m] - 2 * np.outer(vk,vk) @ b[k:m]
-#     R = A
-#     return R
 
 
 #A = np.array([[1, 0],[0, 1],[1, 0]])
 A = np.array([[2., -2.,  18.],[2., 1., 0.],[1., 2., 0.]])
 print('A', A)
-R = HouseHolder_QR(A)
+R = HouseHolder_QR_withoutQ(A)
 print(R)
 
 Q, R = HouseHolder_QR_withQ(A)
